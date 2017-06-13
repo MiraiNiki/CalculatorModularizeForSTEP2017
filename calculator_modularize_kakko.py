40155d2ac2f30f29bf30d9c455e272184dcc1875
Niki Mirai <s1411400@coins.tsukuba.ac.jp>
2017-06-13 19:02:10 +0900

    かっこへの対応

--- calculator_modularize_kakko.py
+++ calculator_modularize_kakko.py
@@ -67,58 +67,57 @@ def tokenize(line):
 
 def executeFourOperations(tokens, depth):
     depthCnt = depth
-    while depthCnt >= 0
+    while depthCnt >= 0:
         index = 1
-        print tokens
         d = 0
         while index < len(tokens):
             if tokens[index]['type'] == 'LeftParentheses':
                 d = tokens[index]['depth']
-                del tokens[index]
-                tokens = insertDummy(tokens, index)
+                tokens = insertDummy(tokens, index + 1)
             elif tokens[index]['type'] == 'RightParentheses':
                 d = tokens[index]['depth'] - 1
-                del tokens[index]
-                index -= 1
             elif depthCnt == d:
                 if tokens[index]['type'] == 'NUMBER':
                     if tokens[index - 1]['type'] == 'MULTIPLY':
                         tokens[index - 2]['number'] *=  tokens[index]['number']
                         del tokens[index - 1:index + 1]
-                        index = index - 1
+                        index = index - 2
                     elif tokens[index - 1]['type'] == 'DIVIDE':
                         tokens[index - 2]['number'] /=  tokens[index]['number']
                         del tokens[index - 1:index + 1]
-                        index = index - 1 
+                        index = index - 2 
             index += 1 
         index = 1
-        print tokens
+        d = 0
         while index < len(tokens):
             if tokens[index]['type'] == 'LeftParentheses':
                 d = tokens[index]['depth']
-                del tokens[index]
-                tokens = insertDummy(tokens, index)
+                if depthCnt == d:
+                    del tokens[index]
+                    index -= 1
             elif tokens[index]['type'] == 'RightParentheses':
-                d = tokens[index]['depth'] - 1
-                del tokens[index]
-                index -= 1
+                if depthCnt == d:
+                    del tokens[index]
+                    index -= 1
+                d -= 1
             elif depthCnt == d:
                 if tokens[index]['type'] == 'NUMBER':
                     if tokens[index - 1]['type'] == 'PLUS':
                         tokens[index - 2]['number'] += tokens[index]['number']
                         del tokens[index - 1:index + 1]
+                        index = index - 2
                     elif tokens[index - 1]['type'] == 'MINUS':
                         tokens[index - 2]['number'] -= tokens[index]['number']
                         del tokens[index - 1:index + 1]
-                    else:
-                        print tokens[index - 1]['type']
+                        index = index - 2
             index += 1
         depthCnt -= 1
-
+    print tokens
     return tokens
 
 def estimateDepth(tokens):
     depth = 0
+    index = 1
     while index < len(tokens):
         if tokens[index]['type'] == 'LeftParentheses':
             if tokens[index]['depth'] > depth:
@@ -157,6 +156,11 @@ def runTest():
     test("2+3*4", 14)
     test("2+3*4/2", 8)
     test("21+3*4/2-300+200*2", 127)
+    test("2*(1+3)", 8)
+    test("(1+4)*3", 15)
+    test("2*(1+3*(2+4))", 38)
+    test("2*(1+3*(2+4*3))", 2*(1+3*(2+4*3)))
+    test("(3*2*(4+2*(1+3)))+2*(1+3*(2+4*3))", (3*2*(4+2*(1+3)))+2*(1+3*(2+4*3)))
     print "==== Test finished! ====\n"
 
 runTest()
